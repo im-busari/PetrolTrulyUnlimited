a1b256595c33871506f2f4ecc35538ab88a9f929
Immanuella Busari <emi.busari@gmail.com>
2020-11-28 16:03:18 +0000

    Add properties to vehicle class

--- PetrolTrulyUnlimited/Pump.cs
+++ PetrolTrulyUnlimited/Pump.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Timers;
 
 namespace PetrolTrulyUnlimited
 {
     class Pump
     {
         private int _id;
-        private int _totalLitresDispensed;
+        private int _totalLitresDispensed; // 1.5 litres/second
         private int _vehicles;
         private int _comission;
         public bool isAvailable = true;
+
+        Random rand = new Random();
 
 
         public Pump(int id)
@@ -24,6 +27,22 @@ namespace PetrolTrulyUnlimited
         public void addReceipt()
         {
             Console.WriteLine("[Vehicle type, Number of litres and Pump number]");
+        }
+
+        public void fillingVehicle(Vehicle vehicle)
+        {
+            int time = rand.Next(17000, 19000);
+            Timer fillingTimer = new Timer(time);
+            fillingTimer.Elapsed += OnTimedEvent;
+            fillingTimer.Enabled = true;
+            fillingTimer.AutoReset = false;
+            fillingTimer.Enabled = true;
+        }
+
+        private void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            this._vehicles += 1;
+            Console.WriteLine(this._vehicles);
         }
     }
 }
--- PetrolTrulyUnlimited/Vehicle.cs
+++ PetrolTrulyUnlimited/Vehicle.cs
@@ -9,21 +9,26 @@ namespace PetrolTrulyUnlimited
         private int _id;
         private string _brand;
         private string _fuelType;
+        private static int _nextID = 0; // to ensure we will not have 2 cars with the same IDs.
+        private string _fuelTime;
 
         private string[] brandsOptions = { "Car", "Van", "HGV" };
         private string[] fuelOptions = { "Unleaded", "Diesel", "LPG" };
 
+        //  A random number that we will use when creating a new vehicle to decide fuel type and brand.
         Random rnd = new Random();
 
-        public Vehicle(int id)
+        public Vehicle(string fuelTime)
         {
-            this._id = id;
+            this._id = _nextID++;
             this._brand = brandsOptions[rnd.Next(brandsOptions.Length)];
             this._fuelType = fuelOptions[rnd.Next(fuelOptions.Length)];
+            this._fuelTime = fuelTime;
         }
 
         public int id { get => this._id; }
         public string brand { get => this._brand; }
         public string fuelType { get => this._fuelType; }
+        public string fuelTime { get => this._fuelTime; }
     }
 }
