cc99402464d2b01e9f20e415767661590ea19671
Immanuella Busari <emi.busari@gmail.com>
2020-11-29 13:47:38 +0000

    Setup basic functionalities for program to run

--- PetrolTrulyUnlimited/Program.cs
+++ PetrolTrulyUnlimited/Program.cs
@@ -5,7 +5,32 @@ namespace PetrolTrulyUnlimited
 {
     class Program
     {
+
         static void Main(string[] args)
+        {
+            PetrolStation.createStation();
+
+            Timer timer = new Timer();
+            timer.Interval = 2050;
+            timer.AutoReset = true; //  every 2 seconds update console view/display
+            timer.Elapsed += RunProgramLoop;
+            timer.Enabled = true;
+            timer.Start();
+
+            Console.ReadLine();
+        }
+
+        //  
+        static void RunProgramLoop(object sender, ElapsedEventArgs e)
+        {
+            Console.Clear();
+            Display.DrawVehicles();
+            Console.WriteLine();
+            Console.WriteLine();
+            Display.DrawPumps();
+            PetrolStation.AssignVehicleToPump();
+        }
+        /*static void Main(string[] args)
         {
             Random rand = new Random();
             Timer vehicleTimer = new Timer();
@@ -22,6 +47,6 @@ namespace PetrolTrulyUnlimited
             }
 
             Console.ReadLine();
-        }
+        }*/
     }
 }
